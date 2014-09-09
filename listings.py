e8ef833ef6de24c29d6903e534e66b1d4ce30931
nyarlathotep <nyarlathotep@Nataraja.debugtheplanet.com>
2014-09-09 01:25:55 -0400

    Threading for the primary listings query.  Note to us: should maybe do this in a reusable fasion, I have a feeling we'll need similar capabilities elsewhere.

--- listings.py
+++ listings.py
@@ -1,5 +1,5 @@
-import json
 import util
+import threading
 
 
 class Listing:
@@ -26,7 +26,7 @@ class ListingsByID:
 
 
 #Inner function.  takes a raw list of listings to query for and returns the response.
-def _innerQueryListingsByID(listing_list):
+def _queryListingsByIDList(listing_list):
 	listing_string = ""
 	if len(listing_list) > 0:
 		for listing_id in listing_list:
@@ -49,8 +49,8 @@ def _innerQueryListingsByID(listing_list):
 
 #Takes an int, string of an int, or enumerable of ints (or ints in strings yadda yadda)
 #returns a dictionary in the form of {id:listingsById}
-def queryListingsByID(listing_ids):
-
+#Also populates the out_dir directory ({}) that you pass in to it.  For use in threaded applications.
+def queryListingsByID_out(listing_ids, out_dir):
 	BATCH_SIZE = 300
 
 	listing_id = None
@@ -70,15 +70,25 @@ def queryListingsByID(listing_ids):
 	while len(listing_list) != 0:
 		listing_batch = listing_list[:BATCH_SIZE]
 
-		index = _innerQueryListingsByID(listing_batch)
+		index = _queryListingsByIDList(listing_batch)
 
 		listing_list = listing_list[BATCH_SIZE:]
 
 		for (key, value) in index.items():
 			accumulated_index[key] = value
 
+			if out_dir != None:
+				#print "Adding:", key, " ", value
+				out_dir[key] = value
+
+	#print "Found:", len(accumulated_index)
 	return accumulated_index
-	
+
+
+#Takes an int, string of an int, or enumerable of ints (or ints in strings yadda yadda)
+#returns a dictionary in the form of {id:listingsById}
+def queryListingsByID(listing_ids):
+	return queryListingsByID_out(listing_ids, None)
 
 
 #Returns a list of all current listing ID's
@@ -89,11 +99,27 @@ def queryListings():
 
 
 #Builds an index of ALL listings.  Be ready for a bit of a wait.
-#TODO; thread this shit yo. (well, multiprocess, I bet http blocks the GIL)
-def deepQueryListings():
+#TODO: Maybe try multiprocessing it? the overhead of starting the other interpreters probably isn't worth it.
+def deepQueryListings(threaded=True):
 
-	listing_ids = queryListings()
+	listing_ids = queryListings()[:1000]
+	listing_index = {}
 
-	listing_index = queryListingsByID(listing_ids[-1000:])
+	if threaded:
+		#Yes I know it can actually start THREADPOOL_SIZE+1 threads, hush.
+		THREADPOOL_SIZE=10
+		threadpool = []
+		for i in range(0, len(listing_ids), len(listing_ids) / THREADPOOL_SIZE):
+			thread = threading.Thread( target=queryListingsByID_out, kwargs={"listing_ids":listing_ids[i : ( i + len(listing_ids) / THREADPOOL_SIZE )], "out_dir":listing_index} )
+			thread.start()
+			threadpool.append(thread)
+		
+
+		for thread in threadpool:
+			thread.join()
+	
+	else:
+		listing_index = queryListingsByID(listing_ids[-1000:])
 
 	return listing_index
+
