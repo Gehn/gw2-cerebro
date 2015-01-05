94013830e8471f980e63b9dd7c5915f67eab09d0
nyarlathotep <nyarlathotep@Nataraja.debugtheplanet.com>
2015-01-05 00:53:07 -0500

    Cleaned up the listing code  a bit.  Made it into objects.  Fixed a ton of bugs too, refactored a bit.  Adjusted the API request to work with Anets new HTTPS, and make it more robust in general to redirects and stuff.

--- listings.py
+++ listings.py
@@ -2,124 +2,201 @@ import util
 import threading
 
 
-class Listing:
+class Offer:
 	def __init__(self, listing_dir = {}):
-		self.listings = None
-		self.unit_price = None
-		self.quantity = None
+		self.listings = 0
+		self.unit_price = 0
+		self.quantity = 0
 
 		if listing_dir and listing_dir != {}:
 			self.listings = listing_dir["listings"]
 			self.unit_price = listing_dir["unit_price"]
 			self.quantity = listing_dir["quantity"]
 
-class ListingsByID:
+
+class ItemListing:
 	def __init__(self, listing_dir = {}):
 		self._id = None
-		self.buys = None
-		self.sells = None
+		self.buys = []
+		self.sells = []
+
+		self.buy_volume = 0
+		self.sell_volume = 0
+
+		self.max_buy = 0
+		self.min_sell = 0
+
+		self.mean_buy = 0
+		self.median_buy = 0
+		self.mode_buy = 0
+
+		self.mean_sell = 0
+		self.median_sell = 0
+		self.mode_sell = 0
 
 		if listing_dir and listing_dir != {}:
 			self._id = listing_dir["id"]
-			self.buys = [Listing(l) for l in listing_dir["buys"]]
-			self.sells = [Listing(l) for l in listing_dir["sells"]]
+
+			#enumerate buys.
+			for listing_raw in listing_dir["buys"]:
+				#Create a offer object for each one.
+				offer = Offer(listing_raw)
+				self.buys.append(offer)
+
+				#Compute the max buy, rolling.
+				if offer.unit_price > self.max_buy:
+					self.max_buy = offer.unit_price
+
+				#Compute the first half of the mean, rolling.
+				self.mean_buy += offer.unit_price * offer.quantity
+
+				# TODO: an interesting thought would be deriving volume from listing count.
+				self.buy_volume += offer.quantity
 
 
-#Inner function.  takes a raw list of listings to query for and returns the response.
-def _queryListingsByIDList(listing_list):
-	listing_string = ""
-	if len(listing_list) > 0:
-		for listing_id in listing_list:
-			listing_string += str(listing_id) + ','
+			#Enumerate sells.
+			for listing_raw in listing_dir["sells"]:
+				#Create a offer object for each one.
+				offer = Offer(listing_raw)
+				self.sells.append(offer)
 
-		listing_string = listing_string[:-1]
+				#Compute the min sell, rolling.
+				if offer.unit_price < self.min_sell:
+					self.min_sell = offer.unit_price
 
-	api_string = '/v2/commerce/listings?ids=' + listing_string
-	listings_response = util.apiCall(api_string)
+				#Compute the first half of the mean, rolling.
+				self.mean_sell += offer.unit_price * offer.quantity
 
-	listings_by_id_index = {}
-	for listings_by_id_dict in listings_response:
-		listings_by_id = ListingsByID(listings_by_id_dict)
-		listings_by_id_index[listings_by_id._id] = listings_by_id
-		
+				self.sell_volume += offer.quantity
 
-	return listings_by_id_index
+			#Calculate the second halves of the means now that we have all listings.
+			if self.sell_volume > 0:
+				self.mean_sell /= self.sell_volume
 
+			if self.buy_volume > 0:
+				self.mean_buy /= self.buy_volume
 
-
-#Takes an int, string of an int, or enumerable of ints (or ints in strings yadda yadda)
-#returns a dictionary in the form of {id:listingsById}
-#Also populates the out_dir directory ({}) that you pass in to it.  For use in threaded applications.
-def queryListingsByID_out(listing_ids, out_dir):
-	BATCH_SIZE = 300
-
-	listing_id = None
-	listing_list = []
-	try:
-		#If it's an int we just cram it in the string
-		listing_id = int(listing_ids)
-		listing_list = [listing_id]
-	except:
-		#If it's a list of ints we build a comma seperated list
-		for listing_id in listing_ids:
-			int(listing_id)
-			listing_list.append(listing_id)
+			#TODO: Calculate the medians.  remember that each listing has multiple quantity.
 
 
-	accumulated_index = {}
-	while len(listing_list) != 0:
-		listing_batch = listing_list[:BATCH_SIZE]
-
-		index = _queryListingsByIDList(listing_batch)
-
-		listing_list = listing_list[BATCH_SIZE:]
-
-		for (key, value) in index.items():
-			accumulated_index[key] = value
-
-			if out_dir != None:
-				#print "Adding:", key, " ", value
-				out_dir[key] = value
-
-	#print "Found:", len(accumulated_index)
-	return accumulated_index
+class Market:
+	def __init__(self):
+		self.item_listings = {}
 
 
-#Takes an int, string of an int, or enumerable of ints (or ints in strings yadda yadda)
-#returns a dictionary in the form of {id:listingsById}
-def queryListingsByID(listing_ids):
-	return queryListingsByID_out(listing_ids, None)
-
-
-#Returns a list of all current listing ID's
-def queryListings():
-
-	return util.apiCall('/v2/commerce/listings')
-
-
-
-#Builds an index of ALL listings.  Be ready for a bit of a wait.
-#TODO: Maybe try multiprocessing it? the overhead of starting the other interpreters probably isn't worth it.
-def deepQueryListings(threaded=True):
-
-	listing_ids = queryListings()[:1000]
-	listing_index = {}
-
-	if threaded:
-		#Yes I know it can actually start THREADPOOL_SIZE+1 threads, hush.
-		THREADPOOL_SIZE=10
-		threadpool = []
-		for i in range(0, len(listing_ids), len(listing_ids) / THREADPOOL_SIZE):
-			thread = threading.Thread( target=queryListingsByID_out, kwargs={"listing_ids":listing_ids[i : ( i + len(listing_ids) / THREADPOOL_SIZE )], "out_dir":listing_index} )
-			thread.start()
-			threadpool.append(thread)
-		
-
-		for thread in threadpool:
-			thread.join()
+	#Inner function.  takes a raw list of listings to query for and returns the response.
+	def _getItemListingsByList(self, listing_list):
+		listing_string = ""
+		if len(listing_list) > 0:
+			for listing_id in listing_list:
+				listing_string += str(listing_id) + ','
 	
-	else:
-		listing_index = queryListingsByID(listing_ids[-1000:])
+			listing_string = listing_string[:-1]
+	
+		api_string = '/v2/commerce/listings?ids=' + listing_string
+		listings_response = util.apiCall(api_string)
 
-	return listing_index
+		listings_by_id_index = {}
+		for listings_by_id_dict in listings_response:
+			listings_by_id = ItemListing(listings_by_id_dict)
+			listings_by_id_index[listings_by_id._id] = listings_by_id
+
+			self.item_listings[listings_by_id._id] = listings_by_id
+			
+
+		return listings_by_id_index
+
+
+
+	def getItemListings_out(self, listing_ids, out_dir):
+		'''
+			Given an int/stringified int (or list of the above), returns a dir
+			of the listings corrosponding to those IDs
+		
+				:param listing_ids: An int/stringified int (or list) of the item listings desired.
+				:param out_dir: Simply pass a {} in, it will be populated with the results.
+		'''
+		BATCH_SIZE = 300
+
+		# Does nastiness to allow many sorts of valid listing_ids types.
+		# e.g. int, stringified int, list of ints and list of stringified ints.
+		listing_id = None
+		listing_list = []
+		try:
+			#If it's an int we just cram it in the string
+			listing_id = int(listing_ids)
+			listing_list = [listing_id]
+		except:
+			#If it's a list of ints we build a comma seperated list
+			for listing_id in listing_ids:
+				int(listing_id)
+				listing_list.append(listing_id)
+
+
+		accumulated_index = {}
+		while len(listing_list) != 0:
+			listing_batch = listing_list[:BATCH_SIZE]
+
+			index = self._getItemListingsByList(listing_batch)
+
+			listing_list = listing_list[BATCH_SIZE:]
+
+			for (key, value) in index.items():
+				accumulated_index[key] = value
+
+				if out_dir != None:
+					#print "Adding:", key, " ", value
+					out_dir[key] = value
+
+		#print "Found:", len(accumulated_index)
+		return accumulated_index
+
+
+	def getItemListings(self, listing_ids):
+		'''
+			Given an int/stringified int (or list of the above), returns a dir
+			of the listings corrosponding to those IDs
+
+				:param listing_ids: An int/stringified int (or list) of the item listings desired.
+		'''
+		return self.getItemListings_out(listing_ids, None)
+
+
+	def getAllListingIDs(self):
+		'''
+			Returns a list of all current listing ID's
+		'''
+		for listing_id in util.apiCall('/v2/commerce/listings'):
+			if int(listing_id) not in self.item_listings:
+				self.item_listings[int(listing_id)] = None
+
+		return self.item_listings.keys()
+
+
+
+	#Builds an index of ALL listings.  Be ready for a bit of a wait.
+	#TODO: Maybe try multiprocessing it? the overhead of starting the other interpreters probably isn't worth it.
+	#TODO: Figure out why serial running doesn't work, but threaded does.  No idea what that's about.
+	def getAllListings(self, threaded=True):
+
+		listing_ids = self.getAllListingIDs()[:1000]
+		listing_index = {}
+
+		if threaded:
+			#Yes I know it can actually start THREADPOOL_SIZE+1 threads, hush.
+			THREADPOOL_SIZE=10
+			threadpool = []
+			for i in range(0, len(listing_ids), len(listing_ids) / THREADPOOL_SIZE):
+				thread = threading.Thread( target=self.getItemListings_out, kwargs={"listing_ids":listing_ids[i : ( i + len(listing_ids) / THREADPOOL_SIZE )], "out_dir":listing_index} )
+				thread.start()
+				threadpool.append(thread)
+			
+
+			for thread in threadpool:
+				thread.join()
+		
+		else:
+			listing_index = self.getItemListings(listing_ids[-1000:])
+
+		return listing_index
 
--- util.py
+++ util.py
@@ -1,16 +1,37 @@
 import httplib
 import json
+from urlparse import urlparse
 
 
 def apiCall(resource):
-	connection = httplib.HTTPConnection("api.guildwars2.com")
-	connection.request("GET", resource)
+	found_resource = False #To deal with redirects
+	url = "api.guildwars2.com"
+	protocol = "http"
+	while not found_resource:
+		print "Getting: ", protocol, url, resource
 
-	response = connection.getresponse()
+		if protocol == "https":
+			connection = httplib.HTTPSConnection(url)
+		else:
+			connection = httplib.HTTPConnection(url)
 
-	if response.status != 200:
-		connection.close()
-		raise Exception("API call failed:" + response.reason)
+		connection.request("GET", resource)
+
+		response = connection.getresponse()
+
+		if response.status >= 400:
+			connection.close()
+			raise Exception("API call failed: " + response.reason + " ; " + str(response.status))
+		elif response.status == 302:
+			parsed_url = urlparse(response.getheader('location'))
+			url = parsed_url.netloc
+			#resource = parsed_url.path FIXME: Determine if I should have this to some extent.
+			protocol = parsed_url.scheme
+			print "Redirecting to: ", protocol, url, resource
+		elif response.status == 200:
+			print "Got response."
+			found_resource = True
+
 
 	response_body = response.read()
 
